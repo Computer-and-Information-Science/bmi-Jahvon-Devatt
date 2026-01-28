c712c3b5e2a00ceaa4483586a4fbb488c69b7a45
Jahvon-Devatt <jahvon.devatt@student.northampton.edu>
2026-01-28 16:06:05 +0000

    Created a program in C++ that calculates a person's BMI.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
- 
+ //Hardcoded values are imperial units
+    int height = 61;
+    int weight = 130;
+
+//Metric units
+    float kg = weight * 0.453592;
+    float meters = height * 0.0254;
+
+    float BMI = kg/(meters * meters);
+
+    cout << "The BMI for this person is: " << BMI; "\n";
 
 
 }
